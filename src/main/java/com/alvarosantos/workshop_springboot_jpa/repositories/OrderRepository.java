2dcb2e015de68cb60c85dc9093cb914964de8593
Alvaro1327 <santosalvaro488@gmail.com>
2026-08-25 11:44:24 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/alvarosantos/workshop_springboot_jpa/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.alvarosantos.workshop_springboot_jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alvarosantos.workshop_springboot_jpa.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
