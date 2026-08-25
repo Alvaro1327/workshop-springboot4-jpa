4ab3ae658376d5b19b3c5d33b97754e8164c02fa
Alvaro1327 <santosalvaro488@gmail.com>
2026-08-25 09:27:49 -0300

    Project creadted

---
+++ src/test/java/com/alvarosantos/workshop_springboot_jpa/WorkshopSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarosantos.workshop_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
