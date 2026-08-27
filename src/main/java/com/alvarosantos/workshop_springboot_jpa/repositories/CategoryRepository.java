6e7a8f1558064850bb74e6f927525b3fb117bb7b
Alvaro1327 <santosalvaro488@gmail.com>
2026-08-27 09:49:11 -0300

    Category

---
+++ src/main/java/com/alvarosantos/workshop_springboot_jpa/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.alvarosantos.workshop_springboot_jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alvarosantos.workshop_springboot_jpa.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
